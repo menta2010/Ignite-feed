f73694eea73054a9fd74b11b4e798c764eb65cae
Natalino Esteves Rodrigues <t2m.nerodrigues@terceiros.mag.com.br>
2022-07-27 00:01:33 -0300

    criando projeto

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import {Post}from './Post';
+export function App() {
+
+  return (
+    <>
+    <Post author = "Natalino Estevs" 
+     content = "Oii" 
+     />
+      <Post author = "lucas Esteves" 
+     content = "olá" 
+     />
+      <Post author = "Natalino Estevs" 
+     content = "tudo bem?" 
+     />
+
+    </>
+  )
+}
+
